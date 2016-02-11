755531f794b4526fbd0c962074c4738ea5b5632f
GrahamAndNick <appacademy@Apps-Mac-mini-100.local>
2016-02-11 13:43:37 -0500

    Initialize board

--- board.rb
+++ board.rb
@@ -1,1 +1,11 @@
 class Board
+  attr_accessor :grid, :size,:mines
+  def initialize(size = 9, mines = 20)
+    @size = size
+    @mines = mines
+    @grid = Array.new(size) { Array.new(size) }
+    populate_board
+  end
+
+
+end
