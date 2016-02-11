a070000bb301af6fcc8fb5756e4a6c32b91af3cb
GrahamAndNick <appacademy@Apps-Mac-mini-100.local>
2016-02-11 13:34:28 -0500

    Add reveal method

--- tiles.rb
+++ tiles.rb
@@ -1,1 +1,24 @@
-gngngh
+class Tile
+  attr_accessor :bomb, :bomb_count
+
+  def initialize(bomb = false)
+     @bomb = bomb
+     @bomb_count = 0 if bomb = false
+  end
+
+  def neighbor_bomb_count
+    self.bomb_count += 1
+  end
+
+  def bombed?
+    bomb
+  end
+
+  def reveal
+    bomb_count
+  end
+
+
+
+
+end
